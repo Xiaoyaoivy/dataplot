babc498b4f8a81564c3374ef2294b855810eb951
huangleping <791616265@qq.com>
2016-08-18 19:43:13 +0800

    origin test

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
-squares = [1,4,9,16,25]
+
+squares = [1, 4, 9, 16, 25]
+plt.plot(squares, linewidth=5)  # 设置线宽
+
+# 设置图表标题，并给坐标轴加上标签
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel('Square of value', fontsize=14)
+
 plt.plot(squares)
 plt.show()
-
