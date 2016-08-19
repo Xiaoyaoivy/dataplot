74dd3f85a9bec17ae9fad5358105d8d209e80858
huangleping <791616265@qq.com>
2016-08-19 19:20:08 +0800

    2016.08.19

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 
+input_value = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares, linewidth=5)  # 设置线宽
+plt.plot(input_value,squares, linewidth=5)  # 设置线宽
 
 # 设置图表标题，并给坐标轴加上标签
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
 plt.ylabel('Square of value', fontsize=14)
 
-plt.plot(squares)
+# 设置刻度标记的大小
+plt.tick_params(axis='both', labelsize=14)
+
+#plt.plot(squares)#默认从0开始
 plt.show()
