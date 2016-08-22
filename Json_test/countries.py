277336a30783d69ee465e7bc852284308767e9a7
huangleping <791616265@qq.com>
2016-08-22 09:36:13 +0800

    2016.08.22

---
+++ Json_test/countries.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# from pygal.maps.world import COUNTRIES
+# from pygal_maps_world import i18n
+from pygal_maps_world.i18n import COUNTRIES
+
+for country_code in sorted(COUNTRIES.keys()):
+    print(country_code,COUNTRIES[country_code])
